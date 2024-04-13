edb4bbf6f195381544c3a9e8a29c8dc9cc19bd95
Savecoders <pablopincay2003@gmail.com>
2024-04-13 13:36:19 -0500

    Refactor UserGuard to use CanActivate interface

--- src/auth/guards/user.guard.ts
+++ src/auth/guards/user.guard.ts
@@ -1,7 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
-export class UserGuard extends AuthGuard('jwt') {}
-// i dont use canActivate method because
-// i want to use this guard in controller level
+export class UserGuard implements CanActivate {
+  constructor(
+    // read the metadata from the controller
+    private readonly reflector: Reflector,
+  ) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as User;
+
+    if (isPublic) return true;
+    if (!user) throw new BadRequestException('User not found (guard)');
+
+    return true;
+  }
+}
