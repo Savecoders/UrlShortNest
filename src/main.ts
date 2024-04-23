a3137c0cc01de010e76b577a9e9798f458cbcb96
Savecoders <pablopincay2003@gmail.com>
2024-04-23 16:43:49 -0500

    Add CORS configuration to main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.enableCors();
   const loger = new Logger('bootstrap');
   const config = app.get(ConfigService);
 
